7fcc692450e923bafe7c3f3f093790eba31c2c2d
Vic Zhang <vzhang@fb.com>
2021-02-03 19:14:51 -0500

    Add table path to HiveTableLayoutHandle
    Table path will be used to construct HdfsContext, which
    may determine how to get access permission.
    
    Co-authored-by: Jalpreet Singh Nanda (:imjalpreet) <jalpreetnanda@gmail.com>

--- presto-hive/src/main/java/com/facebook/presto/hive/HiveMetadata.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/HiveMetadata.java
@@ -2406,11 +2406,16 @@ public class HiveMetadata
         }
 
         TupleDomain<Subfield> domainPredicate = hivePartitionResult.getEffectivePredicate().transform(HiveMetadata::toSubfield);
+        Table table = metastore.getTable(
+                handle.getSchemaTableName().getSchemaName(),
+                handle.getSchemaTableName().getTableName())
+                .orElseThrow(() -> new TableNotFoundException(handle.getSchemaTableName()));
         return ImmutableList.of(new ConnectorTableLayoutResult(
                 getTableLayout(
                         session,
                         new HiveTableLayoutHandle(
                                 handle.getSchemaTableName(),
+                                table.getStorage().getLocation(),
                                 hivePartitionResult.getPartitionColumns(),
                                 // remove comments to optimize serialization costs
                                 pruneColumnComments(hivePartitionResult.getDataColumns()),
@@ -2649,6 +2654,7 @@ public class HiveMetadata
 
         return new HiveTableLayoutHandle(
                 hiveLayoutHandle.getSchemaTableName(),
+                hiveLayoutHandle.getTablePath(),
                 hiveLayoutHandle.getPartitionColumns(),
                 hiveLayoutHandle.getDataColumns(),
                 hiveLayoutHandle.getTableParameters(),
