f4b1ebfdce650fdf6d1b159d32e1072023bf1410
Vic Zhang <vzhang@fb.com>
2021-03-23 20:39:38 -0400

    Add file size and hash code for Presto On Spark
      - File size is utf-8 encoded byte array size.
      - Hash code is calculated using SHA512 algorithm.

--- presto-spark-base/src/test/java/com/facebook/presto/spark/PrestoSparkQueryRunner.java
+++ presto-spark-base/src/test/java/com/facebook/presto/spark/PrestoSparkQueryRunner.java
@@ -375,6 +375,8 @@ public class PrestoSparkQueryRunner
                 Optional.of(sql),
                 Optional.empty(),
                 Optional.empty(),
+                Optional.empty(),
+                Optional.empty(),
                 new TestingPrestoSparkTaskExecutorFactoryProvider(instanceId),
                 Optional.empty(),
                 Optional.empty());
