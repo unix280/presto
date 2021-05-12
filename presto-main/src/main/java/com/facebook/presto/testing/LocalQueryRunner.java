15e57ef8860d455262f764120862a3ab3407263f
Vic Zhang <vzhang@fb.com>
2021-05-12 16:27:16 -0400

    Add properties to update memory in TableFinishOperator
    When system memory used by TableFinishOperator is tracked
    and included in query total memory, query may hit memory
    limit and fail.

--- presto-main/src/main/java/com/facebook/presto/testing/LocalQueryRunner.java
+++ presto-main/src/main/java/com/facebook/presto/testing/LocalQueryRunner.java
@@ -799,6 +799,7 @@ public class LocalQueryRunner
                 joinFilterFunctionCompiler,
                 new IndexJoinLookupStats(),
                 new TaskManagerConfig().setTaskConcurrency(4),
+                new MemoryManagerConfig(),
                 spillerFactory,
                 singleStreamSpillerFactory,
                 partitioningSpillerFactory,
