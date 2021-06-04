c52bdbbf97db1bca68bcb01d5b6c74ae3e2d870a
Shixuan Fan <shixuan@fb.com>
2021-06-03 17:15:09 -0700

    Revert "Support Dwrf Sequence Ids in Writer"
    This reverts commit f6611cbc0ae3dee90e6f5476a2c2aa027999b200.

--- presto-orc/src/main/java/com/facebook/presto/orc/metadata/OrcMetadataWriter.java
+++ presto-orc/src/main/java/com/facebook/presto/orc/metadata/OrcMetadataWriter.java
@@ -35,7 +35,6 @@ import java.io.OutputStream;
 import java.util.List;
 import java.util.Map.Entry;
 
-import static com.facebook.presto.orc.metadata.ColumnEncoding.DEFAULT_SEQUENCE_ID;
 import static com.facebook.presto.orc.metadata.PostScript.MAGIC;
 import static com.google.common.base.Preconditions.checkArgument;
 import static java.lang.Math.toIntExact;
@@ -286,10 +285,6 @@ public class OrcMetadataWriter
 
     private static OrcProto.Stream toStream(Stream stream)
     {
-        checkArgument(
-                stream.getSequence() == DEFAULT_SEQUENCE_ID,
-                "Writing streams with non-zero sequence IDs is not supported in ORC : {} ",
-                stream);
         return OrcProto.Stream.newBuilder()
                 .setColumn(stream.getColumn())
                 .setKind(toStreamKind(stream.getStreamKind()))
@@ -322,8 +317,7 @@ public class OrcMetadataWriter
     {
         checkArgument(
                 !columnEncodings.getAdditionalSequenceEncodings().isPresent(),
-                "Writing columns with non-zero sequence IDs is not supported in ORC: {}",
-                columnEncodings);
+                "Writing columns with non-zero sequence IDs is not supported in ORC: " + columnEncodings);
 
         return OrcProto.ColumnEncoding.newBuilder()
                 .setKind(toColumnEncoding(columnEncodings.getColumnEncodingKind()))
