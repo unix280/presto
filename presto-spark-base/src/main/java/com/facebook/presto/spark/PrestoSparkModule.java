7817742aab16b649894d52f12de1839d0e35b5bf
Andrii Rosa <andriyrosa@gmail.com>
2021-06-18 16:47:26 -0400

    Extend memory exceeded errors with more details
    This is neeeded to simplify out of memory errors debugging. The verbose
    details are completely optional and disabled by default to avoid issues
    related to displaying and storing a verbose error message.
    
    The details contain memory reservation for each operator. Having these
    details would allow us to more easily identify the root cause of a
    memory related failure as it contains the following details:
    
    - Task id
    - Memory allocation breakdown by operator instances
    - Plan Node Id
    - Additional details, such as type of join or whether the distinct
      accumulator is used in hash aggregation
    
    Here is an example of an error message with details:
    
    ```
    Query exceeded per-node total memory limit of 50MB [Allocated: 49.41MB, Delta: 4.12MB, Top Consumers: {HashAggregationOperator=30.85MB, InMemoryHashAggregationBuilder=18.56MB, ScanFilterAndProjectOperator=468B}, Details: [ {
      "taskId" : "0.0.0",
      "reservation" : "49.41MB",
      "topConsumers" : [ {
        "type" : "HashAggregationOperator",
        "planNodeId" : "3",
        "reservations" : [ "9.78MB", "7.79MB", "7.47MB", "2.93MB", "1.81MB", "1.08MB", "0B", "0B", "0B", "0B", "0B", "0B", "0B", "0B", "0B", "0B" ],
        "total" : "30.85MB"
      }, {
        "type" : "HashAggregationOperator",
        "planNodeId" : "138",
        "reservations" : [ "18.56MB" ],
        "total" : "18.56MB"
      }, {
        "type" : "ScanFilterAndProjectOperator",
        "planNodeId" : "153",
        "reservations" : [ "468B" ],
        "total" : "468B"
      } ]
    } ]]
    ```

--- presto-spark-base/src/main/java/com/facebook/presto/spark/PrestoSparkModule.java
+++ presto-spark-base/src/main/java/com/facebook/presto/spark/PrestoSparkModule.java
@@ -93,6 +93,7 @@ import com.facebook.presto.operator.OperatorInfo;
 import com.facebook.presto.operator.OperatorStats;
 import com.facebook.presto.operator.PagesIndex;
 import com.facebook.presto.operator.TableCommitContext;
+import com.facebook.presto.operator.TaskMemoryReservationSummary;
 import com.facebook.presto.operator.index.IndexJoinLookupStats;
 import com.facebook.presto.resourcemanager.NoopResourceGroupService;
 import com.facebook.presto.resourcemanager.ResourceGroupService;
@@ -242,6 +243,7 @@ public class PrestoSparkModule
         jsonCodecBinder(binder).bindJsonCodec(QueryInfo.class);
         jsonCodecBinder(binder).bindJsonCodec(PrestoSparkQueryStatusInfo.class);
         jsonCodecBinder(binder).bindJsonCodec(PrestoSparkQueryData.class);
+        jsonCodecBinder(binder).bindListJsonCodec(TaskMemoryReservationSummary.class);
 
         // smile codecs
         smileCodecBinder(binder).bindSmileCodec(TaskSource.class);
