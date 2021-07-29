d49aed8bc4c13046f70b48975165f832233240f4
Jalpreet Singh Nanda (:imjalpreet) <jalpreetnanda@gmail.com>
2021-07-29 16:31:00 +0530

    AHN-752: Introduce Glue Security Mapping

---
+++ presto-hive-metastore/src/main/java/com/facebook/presto/hive/metastore/glue/GlueSecurityMapping.java
@@ -0,0 +1,71 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.hive.metastore.glue;
+
+import com.facebook.presto.hive.authentication.HiveIdentity;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
+import static com.google.common.base.MoreObjects.toStringHelper;
+import static com.google.common.base.Preconditions.checkArgument;
+import static java.util.Objects.requireNonNull;
+
+public class GlueSecurityMapping
+{
+    // TODO: Add support at group level once user groups are introduced in Presto
+    private final Predicate<String> user;
+    private final String iamRole;
+
+    @JsonCreator
+    public GlueSecurityMapping(
+            @JsonProperty("user") Optional<Pattern> user,
+            @JsonProperty("iamRole") Optional<String> iamRole)
+    {
+        this.user = requireNonNull(user, "user is null")
+                .map(GlueSecurityMapping::toPredicate)
+                .orElse(x -> true);
+        this.iamRole = requireNonNull(iamRole, "iamRole is null").orElse(null);
+
+        checkArgument(iamRole.isPresent(), "must provide iam role");
+    }
+
+    public boolean matches(HiveIdentity identity)
+    {
+        return user.test(identity.getUsername()
+                .orElseThrow(() -> new IllegalStateException("End-user name should exist when metastore impersonation is enabled")));
+    }
+
+    public String getIamRole()
+    {
+        return iamRole;
+    }
+
+    @Override
+    public String toString()
+    {
+        return toStringHelper(this)
+                .add("user", user)
+                .add("iamRole", iamRole)
+                .toString();
+    }
+
+    private static Predicate<String> toPredicate(Pattern pattern)
+    {
+        return value -> pattern.matcher(value).matches();
+    }
+}
---
+++ presto-hive-metastore/src/main/java/com/facebook/presto/hive/metastore/glue/GlueSecurityMappingConfig.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.hive.metastore.glue;
+
+import com.facebook.airlift.configuration.Config;
+import com.facebook.airlift.configuration.ConfigDescription;
+import io.airlift.units.Duration;
+
+import java.io.File;
+import java.util.Optional;
+
+public class GlueSecurityMappingConfig
+{
+    private File configFile;
+    private Duration refreshPeriod;
+
+    public Optional<File> getConfigFile()
+    {
+        return Optional.ofNullable(configFile);
+    }
+
+    @Config("hive.metastore.glue.security-mapping.config-file")
+    @ConfigDescription("JSON configuration file containing glue security mappings")
+    public GlueSecurityMappingConfig setConfigFile(File configFile)
+    {
+        this.configFile = configFile;
+        return this;
+    }
+
+    public Optional<Duration> getRefreshPeriod()
+    {
+        return Optional.ofNullable(refreshPeriod);
+    }
+
+    @Config("hive.metastore.glue.security-mapping.refresh-period")
+    @ConfigDescription("Time interval after which securing mapping configuration will be refreshed")
+    public GlueSecurityMappingConfig setRefreshPeriod(Duration refreshPeriod)
+    {
+        this.refreshPeriod = refreshPeriod;
+        return this;
+    }
+}
