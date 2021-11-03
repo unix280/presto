21c5967838b9f7de51f8b17bbb6de82ee9afc6d0
maobaolong <307499405@qq.com>
2021-11-03 23:45:39 +0800

    Support Caching module for iceberg connector

--- presto-iceberg/src/main/java/com/facebook/presto/iceberg/InternalIcebergConnectorFactory.java
+++ presto-iceberg/src/main/java/com/facebook/presto/iceberg/InternalIcebergConnectorFactory.java
@@ -17,6 +17,7 @@ import com.facebook.airlift.bootstrap.Bootstrap;
 import com.facebook.airlift.bootstrap.LifeCycleManager;
 import com.facebook.airlift.event.client.EventModule;
 import com.facebook.airlift.json.JsonModule;
+import com.facebook.presto.cache.CachingModule;
 import com.facebook.presto.common.type.TypeManager;
 import com.facebook.presto.hive.NodeVersion;
 import com.facebook.presto.hive.authentication.HiveAuthenticationModule;
@@ -69,6 +70,7 @@ public final class InternalIcebergConnectorFactory
                     new HiveAuthenticationModule(),
                     new RubixModule(),
                     new HiveMetastoreModule(catalogName, metastore),
+                    new CachingModule(),
                     binder -> {
                         binder.bind(NodeVersion.class).toInstance(new NodeVersion(context.getNodeManager().getCurrentNode().getVersion()));
                         binder.bind(NodeManager.class).toInstance(context.getNodeManager());
