ccc182d194d3f8387eefa5ca364b770f02585ed9
panyliu <panyliu@tencent.com>
2021-11-12 17:53:25 +0800

    Support iceberg connector concurrent insertion
    Referenced commits:
    https://github.com/apache/iceberg/commit/718b85d3046561b0d046826f2733021a9511099f, https://github.com/apache/iceberg/commit/d5443e3a34a4288441a015ab616d965557d78202

--- presto-hive-metastore/src/main/java/com/facebook/presto/hive/metastore/ExtendedHiveMetastore.java
+++ presto-hive-metastore/src/main/java/com/facebook/presto/hive/metastore/ExtendedHiveMetastore.java
@@ -13,6 +13,7 @@
  */
 package com.facebook.presto.hive.metastore;
 
+import com.facebook.presto.common.NotSupportedException;
 import com.facebook.presto.common.predicate.Domain;
 import com.facebook.presto.common.type.Type;
 import com.facebook.presto.hive.HiveType;
@@ -126,4 +127,14 @@ public interface ExtendedHiveMetastore
     boolean isImpersonationEnabled();
 
     void setPartitionLeases(MetastoreContext metastoreContext, String databaseName, String tableName, Map<String, String> partitionNameToLocation, Duration leaseDuration);
+
+    default long lock(MetastoreContext metastoreContext, String databaseName, String tableName)
+    {
+        throw new NotSupportedException("Lock is not supported by default");
+    }
+
+    default void unlock(MetastoreContext metastoreContext, long lockId)
+    {
+        throw new NotSupportedException("Unlock is not supported by default");
+    }
 }
