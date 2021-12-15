2248fd3d65bc5fe3af94d30868eb1576312f16c9
wangjingdong <silenceeis@163.com>
2021-12-15 20:10:19 +0800

    support grpc udf

--- presto-spark-base/src/test/java/com/facebook/presto/spark/PrestoSparkQueryRunner.java
+++ presto-spark-base/src/test/java/com/facebook/presto/spark/PrestoSparkQueryRunner.java
@@ -320,6 +320,7 @@ public class PrestoSparkQueryRunner
                         "unittest",
                         new SqlFunctionExecutors(
                                 ImmutableMap.of(SQL, FunctionImplementationType.SQL),
+                                null,
                                 null),
                         new SqlInvokedFunctionNamespaceManagerConfig().setSupportedFunctionLanguages("sql")));
 
