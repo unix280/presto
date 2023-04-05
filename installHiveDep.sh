5655922b0137809d443d623d309938b205edc716
Haritha K <HARITHA.K@ibm.com>
2023-04-05 12:43:34 +0530

    Changing installHiveDep.sh for mvnw support

--- installHiveDep.sh
+++ installHiveDep.sh
@@ -10,8 +10,8 @@ else
 fi
 cd presto-hive4-apache/
 echo "Building presto-hive4-apache..."
-mvn clean install
+./mvnw clean install
 echo "Cleaning up the hive-apache-ibm JAR..."
 ./refactorJar.sh
-mvn install:install-file -Dfile=target/hive-apache-ibm-3.0.0-8.jar -DgroupId=com.facebook.presto.hive -DartifactId=hive-apache-ibm -Dversion=3.0.0-8 -Dpackaging=jar -DgeneratePom=true
+./mvnw install:install-file -Dfile=target/hive-apache-ibm-3.0.0-8.jar -DgroupId=com.facebook.presto.hive -DartifactId=hive-apache-ibm -Dversion=3.0.0-8 -Dpackaging=jar -DgeneratePom=true
 echo "hive-apache-ibm JAR installed to .m2"
