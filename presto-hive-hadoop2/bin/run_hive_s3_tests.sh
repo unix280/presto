a62f25c0eee53127c06dccea1a017b2a9fd7743a
Jalpreet Singh Nanda (:imjalpreet) <jalpreetnanda@gmail.com>
2023-05-04 16:28:55 -0700

    Fix AWS_S3_BUCKET env variable name in run_hive_s3_tests.sh

--- presto-hive-hadoop2/bin/run_hive_s3_tests.sh
+++ presto-hive-hadoop2/bin/run_hive_s3_tests.sh
@@ -28,7 +28,7 @@ exec_in_hadoop_master_container /usr/bin/hive -e "
     CREATE EXTERNAL TABLE presto_test_external_fs(t_bigint bigint)
     LOCATION '${table_path}'"
 
-table_path="s3a://${S3_BUCKET}/presto_test_external_fs_comma_delimited/"
+table_path="s3a://${AWS_S3_BUCKET}/presto_test_external_fs_comma_delimited/"
 exec_in_hadoop_master_container hadoop fs -mkdir -p "${table_path}"
 exec_in_hadoop_master_container hadoop fs -put -f /tmp/files/test_comma_delimited_table.csv{,.gz,.bz2} "${table_path}"
 exec_in_hadoop_master_container /usr/bin/hive -e "
@@ -38,7 +38,7 @@ exec_in_hadoop_master_container /usr/bin/hive -e "
     STORED AS TEXTFILE
     LOCATION '${table_path}'"
 
-table_path="s3a://${S3_BUCKET}/presto_test_external_fs_pipe_delimited/"
+table_path="s3a://${AWS_S3_BUCKET}/presto_test_external_fs_pipe_delimited/"
 exec_in_hadoop_master_container hadoop fs -mkdir -p "${table_path}"
 exec_in_hadoop_master_container hadoop fs -put -f /tmp/files/test_pipe_delimited_table.csv{,.gz,.bz2} "${table_path}"
 exec_in_hadoop_master_container /usr/bin/hive -e "
