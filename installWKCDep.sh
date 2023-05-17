db1bf341a335786571a684405da88119c2e158cc
Ben Tony Joe <bentonyjoe@ibm.com>
2023-05-18 00:21:22 +0530

    dependency changes

--- installWKCDep.sh
+++ installWKCDep.sh
@@ -9,7 +9,8 @@ else
   git clone git@github.ibm.com:lakehouse/wkc-libs.git
 fi
 cd wkc-libs/
+chmod 777 *
 echo "Starting JAR Installation .m2"
-mvn install:install-file -Dfile=libs/lts-masking-0.3.9.jar -DgroupId=com.ibm.dp -DartifactId=lts-masking -Dversion=0.3.9 -Dpackaging=jar -DgeneratePom=true
-mvn install:install-file -Dfile=libs/wdp-policy-service-sdk-3.5.1227.jar -DgroupId=com.ibm.wdp.policy -DartifactId=wdp-policy-service-sdk -Dversion=3.5.1227 -Dpackaging=jar -DgeneratePom=true
+./mvnw install:install-file -Dfile=libs/lts-masking-0.3.9.jar -DgroupId=com.ibm.dp -DartifactId=lts-masking -Dversion=0.3.9 -Dpackaging=jar -DgeneratePom=true
+./mvnw install:install-file -Dfile=libs/wdp-policy-service-sdk-3.5.1227.jar -DgroupId=com.ibm.wdp.policy -DartifactId=wdp-policy-service-sdk -Dversion=3.5.1227 -Dpackaging=jar -DgeneratePom=true
 echo "presto-wkc-dep JAR installed to .m2"
