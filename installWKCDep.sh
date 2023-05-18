209bb8259ab71bd65e7a02065c8ca6df329701cc
Ben Tony Joe <bentonyjoe@ibm.com>
2023-05-18 11:58:10 +0530

    permission changes

--- installWKCDep.sh
+++ installWKCDep.sh
@@ -9,7 +9,7 @@ else
   git clone git@github.ibm.com:lakehouse/wkc-libs.git
 fi
 cd wkc-libs/
-chmod 777 *
+chmod 755 *
 echo "Starting JAR Installation .m2"
 ./mvnw install:install-file -Dfile=libs/lts-masking-0.3.9.jar -DgroupId=com.ibm.dp -DartifactId=lts-masking -Dversion=0.3.9 -Dpackaging=jar -DgeneratePom=true
 ./mvnw install:install-file -Dfile=libs/wdp-policy-service-sdk-3.5.1227.jar -DgroupId=com.ibm.wdp.policy -DartifactId=wdp-policy-service-sdk -Dversion=3.5.1227 -Dpackaging=jar -DgeneratePom=true
