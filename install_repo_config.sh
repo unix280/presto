52f4889e8207f9a8be973e71de7350d423927b71
Joffin Johnson <Joffin.Johnson1@ibm.com>
2023-05-21 11:49:42 +0530

    Update install_repo_config.sh

--- install_repo_config.sh
+++ install_repo_config.sh
@@ -1,7 +1,7 @@
 if [ "$artifactory_user_enabled" = true ]
 then
     echo "============================ Working as artifactory user =================================="
-    sh update_settings.sh
+    #sh update_settings.sh
 else
     echo "============================ Working as developer user =================================="
     sh installWKCDep.sh
