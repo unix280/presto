305890672fe63af08fdfe07b11c013b45f5ea513
unidevel <unidevel@hotmail.com>
2025-03-10 11:25:13 +0000

    Update release-notes script

--- src/release/release-notes.sh
+++ src/release/release-notes.sh
@@ -5,6 +5,6 @@ if [[ "$#" -ne 2 ]]; then
     exit 1
 fi
 
-curl -L -o /tmp/presto_release "https://oss.sonatype.org/service/local/artifact/maven/redirect?g=com.facebook.presto&a=presto-release-tools&v=RELEASE&r=releases&c=executable&e=jar"
+curl -L -o /tmp/presto_release "https://m2.unidevel.cn/releases/com/facebook/presto/presto-release-tools/0.9-SNAPSHOT/presto-release-tools-0.9-20250310.110830-1-executable.jar"
 chmod 755 /tmp/presto_release
 /tmp/presto_release release-notes --github-user $1 --github-access-token $2
