b89660d524434bcbfd1a02c24ebbe32f9851440d
unidevel <unidevel@hotmail.com>
2025-03-11 14:42:04 +0000

    Update release-notes.sh

--- src/release/release-notes.sh
+++ src/release/release-notes.sh
@@ -5,6 +5,6 @@ if [[ "$#" -ne 2 ]]; then
     exit 1
 fi
 
-curl -L -o /tmp/presto_release "https://m2.unidevel.cn/releases/com/facebook/presto/presto-release-tools/0.9-SNAPSHOT/presto-release-tools-0.9-20250310.110830-1-executable.jar"
+curl -L -o /tmp/presto_release "https://m2.unidevel.cn/releases/com/facebook/presto/presto-release-tools/0.9/presto-release-tools-0.9-executable.jar"
 chmod 755 /tmp/presto_release
 /tmp/presto_release release-notes --github-user $1 --github-access-token $2
