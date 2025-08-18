73e0a9d6418b2bc2ac2f9d83a7efd344fc66f3ab
jacobk <jacobk@ibm.com>
2025-08-18 14:39:39 -0700

    Add E2E tests for Geo Functions
    Alchemy-item: (ID = 1028) Add E2E tests for Geo Functions commit 1/1 - 981d7e23ea7bda0a290d3e5a2ef39a45d7f28660

--- presto-tests/src/main/java/com/facebook/presto/tests/QueryAssertions.java
+++ presto-tests/src/main/java/com/facebook/presto/tests/QueryAssertions.java
@@ -417,7 +417,8 @@ public final class QueryAssertions
             }
         }
         else {
-            if (!(exact ? nullToEmpty(exception.getMessage()).equals(regex) : nullToEmpty(exception.getMessage()).matches(regex))) {
+            if (exact ? !nullToEmpty(exception.getMessage()).equals(regex) && !exception.getMessage().equals(regex)
+                    : !nullToEmpty(exception.getMessage()).matches(regex) && !exception.getMessage().matches(regex)) {
                 fail(format("Expected exception message '%s' to match '%s' for query: %s", exception.getMessage(), regex, sql), exception);
             }
         }
